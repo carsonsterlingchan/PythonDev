cd4cf6896d92bfac2425af28fdae77cb6f3e5636
carsonsterlingchan <carson@carsonsterlingchan.com>
2015-12-10 12:31:00 -0500

    Fixed the issue.  The program finally works.

--- find_pairs.py
+++ find_pairs.py
@@ -2,6 +2,8 @@
 # output
 # 1,5
 # 2,4
+	
+	
 
 def find_pairs(input_list, the_sum):
 	
@@ -10,13 +12,22 @@ def find_pairs(input_list, the_sum):
 	
 	for i in input_list:
 		diff = the_sum - i
-		print i 
-		print i in pair_dict.values()
-		if diff in input_list and not (i in pair_dict and i in pair_dict.values()):
+		#print i 
+		# print i in pair_dict.values()
+		if diff in input_list and (not (i in pair_dict and diff in pair_dict.values()) and \
+									not (diff in pair_dict and i in pair_dict.values())) and (i != diff):
 			pair_dict[i] = diff
-			print str(i) + ',' + str(diff)
-			print pair_dict
+			#print str(i) + ',' + str(diff)
+	
+	if len(pair_dict) == 0:
+		print 'No pairs found'
+		return
+	
+	for pair in pair_dict:
+		print str(pair) + "," + str(pair_dict[pair])
 			
 if __name__ == '__main__':
 	input_list = [1,2,4,3,5,6]
-	find_pairs(input_list, 6)
+	find_pairs(input_list, 1)
+	#input_list = [-5,10,2,3,-1,6]
+	#find_pairs(input_list, 4)
